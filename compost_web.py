ceeb748dee268547596956097c23df07a6711951
Dan Higgins <daniel.higgins@yahoo.com>
2018-10-31 07:58:01 -0400

    udates for datetime format

--- compost_web.py
+++ compost_web.py
@@ -1,8 +1,7 @@
 
-from flask import Flask, request, session, redirect, url_for, render_template, flash
+from flask import Flask, render_template
 from compost_db import CompostDB
 from datetime import datetime
-from dateutil import tz
 import time
 import logging
 
@@ -10,10 +9,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 app = Flask(__name__)  # create the application instance :)
-app.config.from_object(__name__)  # load config from this file , flaskr.py
 
-
-# Retieve data from 'static' directory. Used most typically for rendering images.
 @app.route('/<path:path>')
 def static_file(path):
     return app.send_static_file(path)
@@ -22,23 +18,17 @@ def static_file(path):
 @app.route('/index')
 def index():
     compost_db = CompostDB()
-    temperature, battery, latest_date = compost_db.select_latest_temp_data()
-    time_stamp = int(time.mktime(datetime.strptime(latest_date, "%Y-%m-%d %H:%M:%S").timetuple()))
-    ui_data = {'temperature':temperature,'battery_level':battery,'latest_date':latest_date,'time_stamp':time_stamp}
+    temperature, battery, latest_date_str = compost_db.select_latest_temp_data()
+    latest_date = datetime.strptime(latest_date_str, "%Y-%m-%d %H:%M:%S")
+    time_stamp = int(time.mktime(latest_date.timetuple()))
+    latest_date_str = latest_date.strftime("%m/%d/%Y %I:%M:%S %p")
 
+    ui_data = {'temperature':temperature,'battery_level':battery,'latest_date':latest_date_str,'time_stamp':time_stamp}
 
     logging.debug("ui_data [{}]".format(ui_data))
 
     return render_template('index.html', ui_data=ui_data)
 
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='password',
-    WTF_CSRF_ENABLED=True,
-))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- run.sh
+++ run.sh
@@ -7,14 +7,16 @@ if [ -d "/home/ubuntu/Applications/python_envs" ]; then
     source /home/ubuntu/Applications/python_envs/bin/activate
 fi
 
-nohup ./compost_mqtt.py > compost_mqtt.log 2>&1 &
-echo $! > compost_mqtt.pid
+#nohup ./compost_mqtt.py > compost_mqtt.log 2>&1 &
+#echo $! > compost_mqtt.pid
 
+#Update the iptable to redirect traffic from port 80 to 8080
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 exec gunicorn compost_web:app \
 --workers $NUM_WORKERS \
 --worker-class gevent \
 --timeout $TIMEOUT \
 --log-level=debug \
---bind=0.0.0.0:9000 \
+--bind=0.0.0.0:8080 \
 --pid=$PIDFILE
